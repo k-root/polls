47d612f7db1b2444b63323bc9c81a92ada3b4de7
Kaushik <koiladakaushik@gmail.com>
2017-08-08 11:51:10 +0530

    first commit

---
+++ polls/views.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import get_object_or_404, render
+
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+# Create your views here.
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+from .models import Question, Choice
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
+def vote(request, question_id):
+	question=get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice=question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html',{'question':question, 'error_message':"Select a Choice.. You didn't select any!!",})
+	else:
+		selected_choice.votes +=1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
